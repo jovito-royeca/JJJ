5fd9c0c6cbb8678d753109a430a5a7ff9e9a808e
Jovito Royeca <buttmanizer@Nataesha.local>
2014-02-02 22:08:48 +0800

    Added new JJJ Framework code.

---
+++ JJJ/JJJ/JJJUtil.h
@@ -0,0 +1,23 @@
+//
+//  JJJUtil.h
+//  JJJ
+//
+//  Created by Jovito Royeca on 11/19/13.
+//  Copyright (c) 2013 Jovito Royeca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JJJUtil : NSObject
+
++ (BOOL)string:(NSString *)string containsString:(NSString*)x;
++ (BOOL)isEmptyString:(NSString*)string;
++ (BOOL)isAlphaStart:(NSString*)string;
++ (NSString*)arrayToString:(NSArray*)arr;
++ (NSString*)toUTF8:(NSString*)string;
++ (NSString*)toASCII:(NSString*)string;
++ (NSString*)trim:(NSString*)string;
++ (NSString*)superScriptOf:(NSString*)string;
++ (NSString*)subScriptOf:(NSString*)string;
+
+@end
