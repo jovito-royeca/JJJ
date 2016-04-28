7a05acace9f827ec946544195504c9e0967b6a1f
jovito-royeca <jovit.royeca@gmail.com>
2016-04-28 11:01:32 +0800

    feat: Added new method UIImage+AverageColor -inverseColor:

--- Example/JJJUtils/JJJViewController.m
+++ Example/JJJUtils/JJJViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIColor *redColor = [UIColor redColor];
+//    UIColor *inverseSolor = redColor in
 }
 
 - (void)didReceiveMemoryWarning
